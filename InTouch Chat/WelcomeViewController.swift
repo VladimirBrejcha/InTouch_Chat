f07326352ef23773ee599849f89cafb2fa2e4b72
Vladimir Korolev <python2python@icloud.com>
2019-04-26 13:05:11 +0300

    Added credits

--- InTouch Chat/WelcomeViewController.swift
+++ InTouch Chat/WelcomeViewController.swift
@@ -15,6 +15,8 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var creditsImageView: UIImageView!
+    @IBOutlet weak var backgroundView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,16 @@ class WelcomeViewController: UIViewController {
         if Auth.auth().currentUser != nil {
             performSegue(withIdentifier: "goToChat", sender: self)
         }
+    }
+    @IBAction func creditsButtonPressed(_ sender: Any) {
+        if creditsImageView.isHidden == true {
+            creditsImageView.isHidden = false
+            backgroundView.alpha = 0.5
+        } else {
+            creditsImageView.isHidden = true
+            backgroundView.alpha = 0.85
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
